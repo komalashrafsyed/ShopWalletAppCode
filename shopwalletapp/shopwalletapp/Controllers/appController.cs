4abd254c1ebd7ca9e2b3350298b11c6e935029b6
Abdulmanan <abdulmananyousaf66@gmail.com>
2019-05-05 03:20:05 +0500

    login and register

---
+++ shopwalletapp/shopwalletapp/Controllers/appController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shopwalletapp.Controllers
+{
+    public class appController : Controller
+    {
+        // GET: app
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Customer
+        public ActionResult Customer()
+        {
+            return View();
+        }
+        // GET: Customer
+        public ActionResult login()
+        {
+            return View();
+        }
+        // GET: Customer
+        public ActionResult Payroll()
+        {
+            return View();
+        }
+        // GET: Customer
+        public ActionResult Vendors()
+        {
+            return View();
+        }
+    }
+}
